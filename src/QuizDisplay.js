00de85b3915585fa498819a1bcf9cdcd722c809e
woodelin <woodelin@woodelins-MacBook-Pro.local>
2019-06-18 12:10:54 -0400

    great work

--- src/QuizDisplay.js
+++ src/QuizDisplay.js
@@ -9,7 +9,39 @@ class QuizDisplay extends Renderer {
     // if quiz is inactive and no questions are asked yet, then
     // we're at the intro state of app
     if (!this.model.active && this.model.askedQuestions.length === 0) {
-      return `<div>Intro Screen</div>`;
+      return `
+          <p> Welcome to our trivia quiz. Test your smart and see how high you can score. </p>
+          <button class = "start"> start </button>
+      `;
+    }
+    if(this.model.active && this.model.getCurrentQuestion().answerStatus() === -1){
+        return `
+      <span> ${this.getCurrentQuestion().text}</span>
+          <div>
+              <input type="radio" id="huey" name="drone" value="huey"
+                checked>
+              <label for="huey">${this.model.getCurrentQuestion()}</label>
+              </div>
+
+              <div>
+              <input type="radio" id="dewey" name="drone" value="dewey">
+              <label for="dewey">${this.model.getCurrentQuestion()}</label>
+              </div>
+
+              <div>
+              <input type="radio" id="louie" name="drone" value="louie">
+              <label for="louie">${this.model.getCurrentQuestion()}</label>
+              </div>
+
+
+              <div>
+              <input type="radio" id="louie" name="drone" value="louie">
+              <label for="louie">${this.model.getCurrentQuestion()}</label>
+          </div>
+
+        
+        
+        `
     }
   }
 
@@ -18,15 +50,40 @@ class QuizDisplay extends Renderer {
   */
   getEvents() {
     return {
-      'click .start': 'handleStart'
+      'click .start': 'handleStart',
+      'click .submitAnswer': 'handleSubmitAnswer',
+      'click .continue': 'handleNext',
+      'click .playAgain': 'handlePlayAgain'
     };
   }
+
+  handleSubmitAnswer (event){
+    event.preventDefault();
+
+  }
+
+
+  handleNext (event){
+    event.preventDefault();
+
+  }
+
+
+  handlePlayAgain (event){
+    event.preventDefault();
+
+  }
+
+  
+
+
 
   /**
   * All event handler functions should call model methods
   */ 
-  handleStart() {
-    this.model.startQuiz();
+  handleStart(event) {
+    event.preventDefault();
+    this.model.start();
   }
 }
 
