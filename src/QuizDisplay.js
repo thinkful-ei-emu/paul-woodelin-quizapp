58a2c63e0780e05236efe13f8a12ee1792da369d
woodelin <woodelin@woodelins-MacBook-Pro.local>
2019-06-18 12:51:57 -0400

    great teamwork

--- src/QuizDisplay.js
+++ src/QuizDisplay.js
@@ -8,6 +8,9 @@ class QuizDisplay extends Renderer {
   template() {
     // if quiz is inactive and no questions are asked yet, then
     // we're at the intro state of app
+
+    let currentQ=this.getCurrentQuestion();
+
     if (!this.model.active && this.model.askedQuestions.length === 0) {
       return `
           <p> Welcome to our trivia quiz. Test your smart and see how high you can score. </p>
@@ -15,7 +18,7 @@ class QuizDisplay extends Renderer {
       `;
     }
     if(this.model.active && this.model.getCurrentQuestion().answerStatus() === -1){
-      let currentQ=this.getCurrentQuestion();
+      
       let html=`<span> ${currentQ.text}</span>`;
       for(let i=0;i<currentQ.answers.length;i++){
         html+=`
@@ -25,7 +28,40 @@ class QuizDisplay extends Renderer {
       html+='<button class="submitAnswer"> Submit </button>';
       return html;
         
-    }
+    } if(this.model.active && this.model.getCurrentQuestion().answerStatus() === 1){
+      return `
+      
+      <span> ${currentQ.text} </span> 
+      
+      <p> You got it !. the correct answer was</p>  
+      
+      <span> ${currentQ.correctAnswer} </span>
+      
+      <button class = "continue">continue</button>
+      `
+    } if(this.model.active && this.model.getCurrentQuestion().answerStatus() === 0){
+      return `
+      <span> ${currentQ.text} </span> 
+
+      <p> Sorry that was incorrect. you answered</p>
+
+      <span> ${currentQ.userAnswer}</span>
+
+      <p> The correct answer was</p>
+
+      <span> ${currentQ.correctAnswer} </span>
+
+      <button class = "continue">continue</button>
+      `
+    } if (!this.model.active && this.model.scoreHistory.length > 0){
+         let html =  `
+         <p> Good Job! your final score was ${this.model.score} out of ${this.score.correctAnswer} </p>         
+         `
+
+         if(this)
+         
+    }      
+    
   }
 
   /**
